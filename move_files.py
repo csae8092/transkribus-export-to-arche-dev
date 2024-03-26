be4d4c7c5da96ec1e119eeb7b06ed098701413a9
csae8092 <peter.andorfer@oeaw.ac.at>
2024-03-26 07:49:29 +0100

    added move_files.py to remove the doc_title_directory

--- move_files.py
+++ move_files.py
@@ -1,4 +1,14 @@
+import os
 import shutil
 import glob
+from tqdm import tqdm
 
-files = glob.glob("exports/*/*/**/*.xml")
+files = glob.glob("exports/*/**/*.*", recursive=True)
+for x in tqdm(files):
+    doc_name = x.split("/")[2]
+    to_delete_dir = os.path.join(*x.split("/")[:3])
+    new_path = x.replace(f"{doc_name}/", "")
+    new_path_dir, _ = os.path.split(new_path)
+    os.makedirs(new_path_dir, exist_ok=True)
+    shutil.move(x, new_path)
+    shutil.rmtree(to_delete_dir, ignore_errors=True)
