b33bac85fe52eeb3b41c668f19e0f7c8cacb4729
csae8092 <peter.andorfer@oeaw.ac.at>
2024-03-27 16:07:38 +0100

    added filechecker script

---
+++ filechecker.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+composer require -W acdh-oeaw/repo-file-checker:^3.1.5
+directoryToWriteReportsInto="fc_out"
+
+echo "run ${directoryToWriteReportsInto}"
+rm -rf ${directoryToWriteReportsInto}
+mkdir -p ${directoryToWriteReportsInto}
+vendor/bin/arche-filechecker --csv --html exports ${directoryToWriteReportsInto}
