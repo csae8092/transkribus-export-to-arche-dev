20b8e2a4541ab9ae48d8b38da41ee2bec0f788fa
csae8092 <peter.andorfer@oeaw.ac.at>
2024-03-28 14:18:11 +0100

    minor

--- import_binaries.sh
+++ import_binaries.sh
@@ -3,4 +3,4 @@
 composer require -W acdh-oeaw/arche-ingest:^1.4.6
 
 echo "ingesting binaries"
-vendor/bin/arche-import-binary exports https://id.acdh.oeaw.ac.at/my-transkribus-play/ http://127.0.0.1/api username password --skip not_exist
+vendor/bin/arche-import-binary exports https://id.acdh.oeaw.ac.at/schnitzler-zeitungen/ http://127.0.0.1/api username password --skip not_exist
